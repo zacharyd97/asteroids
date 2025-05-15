ff6984c9504af379864376e73fddfc14b0a2e27f
zacharyd97 <zdowell97@gmail.com>
2025-05-14 22:26:34 -0400

    added class for objects

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    black = 0,0,0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        screen.fill("black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
